90506edb1d778d71115c98065102fbe7b2c868fc
nepdavis <ndavis2052@gmail.com>
2018-01-19 14:57:47 -0500

    Changing formatting to PEP8

--- Projects/proj01/alphabetizer.py
+++ Projects/proj01/alphabetizer.py
@@ -1,38 +1,40 @@
-	
 def order_first_name(a, b):
-	"""
-	Orders two people by their first names
-	:param a: a Person
-	:param b: a Person
-	:return: True if a comes before b alphabetically and False otherwise
-	"""
-	return False
-	
+    """
+    Orders two people by their first names
+    :param a: a Person
+    :param b: a Person
+    :return: True if a comes before b alphabetically and False otherwise
+    """
+    return False
+
+
 def order_last_name(a, b):
-	"""
-	Orders two people by their last names
-	:param a: a Person
-	:param b: a Person
-	:return: True if a comes before b alphabetically and False otherwise
-	"""
-	return False
+    """
+    Orders two people by their last names
+    :param a: a Person
+    :param b: a Person
+    :return: True if a comes before b alphabetically and False otherwise
+    """
+    return False
+
 
 def is_alphabetized(roster, ordering):
-	"""
-	Checks whether the roster of names is alphabetized in the given order
-	:param roster: a list of people
-	:param ordering: a function comparing two elements
-	:return: True if the roster is alphabetized and False otherwise
-	"""
-	return False
-	
+    """
+    Checks whether the roster of names is alphabetized in the given order
+    :param roster: a list of people
+    :param ordering: a function comparing two elements
+    :return: True if the roster is alphabetized and False otherwise
+    """
+    return False
+
+
 def alphabetize(roster, ordering):
-	"""
-	Alphabetizes the roster according to the given ordering
-	:param roster: a list of people
-	:param ordering: a function comparing two elements
-	:return: a sorted version of roster
-	:return: the number of comparisons made
-	"""
-	return (list(roster), 0)
+    """
+    Alphabetizes the roster according to the given ordering
+    :param roster: a list of people
+    :param ordering: a function comparing two elements
+    :return: a sorted version of roster
+    :return: the number of comparisons made
+    """
+    return list(roster), 0
 
