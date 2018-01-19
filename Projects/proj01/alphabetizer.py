70c4db12ab4d1fa51329916f6eabaedb73ae5d8c
nepdavis <ndavis2052@gmail.com>
2018-01-19 15:47:43 -0500

    Added merge sort barebones to alphabetize method

--- Projects/proj01/alphabetizer.py
+++ Projects/proj01/alphabetizer.py
@@ -1,24 +1,37 @@
 def order_first_name(a, b):
+
     """
     Orders two people by their first names
     :param a: a Person
     :param b: a Person
     :return: True if a comes before b alphabetically and False otherwise
     """
+
+    if a.first() < b.first():
+
+        return True
+
     return False
 
 
 def order_last_name(a, b):
+
     """
     Orders two people by their last names
     :param a: a Person
     :param b: a Person
     :return: True if a comes before b alphabetically and False otherwise
     """
+
+    if a.last() < b.last():
+
+        return True
+
     return False
 
 
 def is_alphabetized(roster, ordering):
+
     """
     Checks whether the roster of names is alphabetized in the given order
     :param roster: a list of people
@@ -29,6 +42,7 @@ def is_alphabetized(roster, ordering):
 
 
 def alphabetize(roster, ordering):
+
     """
     Alphabetizes the roster according to the given ordering
     :param roster: a list of people
@@ -36,5 +50,35 @@ def alphabetize(roster, ordering):
     :return: a sorted version of roster
     :return: the number of comparisons made
     """
-    return list(roster), 0
 
+    comparisons = 0
+
+    top = roster[:len(roster)//2]
+
+    bottom = roster[len(roster)//2:]
+
+    all = [None for _ in range(len(roster))]
+
+    i, j = 0, 0
+
+    k = len(roster)
+
+    while i < len(top) and j < len(bottom):
+
+        if top[i] < bottom[j]:
+
+            all[k] = top[i]
+
+            comparisons += 1
+            k += 1
+            i += 1
+
+        else:
+
+            all[k] = bottom[j]
+
+            comparisons += 1
+            k += 1
+            j += 1
+
+    return list(roster), 0
