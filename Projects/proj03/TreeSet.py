43e9b415d651fe013b96226104ca3c0ea12ba369
Nate Davis <ndavis2052@gmail.com>
2018-02-26 21:58:51 -0500

    Formatting to PEP8

--- Projects/proj03/TreeSet.py
+++ Projects/proj03/TreeSet.py
@@ -1,17 +1,21 @@
+
 class TreeSet:
+
     """
     A set data structure backed by a tree.
     Items will be stored in an order determined by a comparison
     function rather than their natural order.
     """
+
     def __init__(self, comp):
+
         """
         Constructor for the tree set.
         You can perform additional setup steps here
         :param comp: A comparison function over two elements
         """
+
         self.comp = comp
-        # added stuff below
 
     def __len__(self):
         return 0
@@ -20,7 +24,7 @@ class TreeSet:
         return -1
 
     def insert(self, item):
-    	return False
+        return False
 
     def remove(self, item):
         return False
@@ -29,10 +33,10 @@ class TreeSet:
         return False
 
     def first(self):
-    	raise KeyError
+        raise KeyError
 
     def last(self):
-    	raise KeyError
+        raise KeyError
 
     def clear(self):
         pass
@@ -43,41 +47,47 @@ class TreeSet:
     # Pre-defined methods
 
     def is_empty(self):
+
         """
         Determines whether the set is empty
         :return: False if the set contains no items, True otherwise
         """
+
         return len(self) == 0
 
     def __repr__(self):
+
         """
         Creates a string representation of this set using an in-order traversal.
         :return: A string representing this set
         """
+
         return 'TreeSet([{0}])'.format(','.join(str(item) for item in self))
 
-    # Helper functions
-    # You can add additional functions here
 
 class TreeNode:
+
     """
     A TreeNode to be used by the TreeSet
     """
+
     def __init__(self, data):
+
         """
         Constructor
         You can add additional data as needed
         :param data:
         """
+
         self.data = data
         self.left = None
         self.right = None
-        # added stuff below
 
     def __repr__(self):
+
         """
         A string representing this node
         :return: A string
         """
-        return 'TreeNode({0})'.format(self.data)
 
+        return 'TreeNode({0})'.format(self.data)
